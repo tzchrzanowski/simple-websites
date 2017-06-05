52be1beb329eae318fbad8a1cda49ecddb08c76c
Tom <tzchrzanowski@gmail.com>
2017-06-05 19:32:02 +0200

    Create http-requests.js

---
+++ http-requests.js
@@ -0,0 +1,10 @@
+// 1 way:
+//http://cors.io/?http://retroarcade.co.nf/products.json
+
+http://cors.io/?
+http://cors.io/?u=
+
+// 2nd way:
+//https://crossorigin.me/http://retroarcade.co.nf/products.json
+
+https://crossorigin.me/
